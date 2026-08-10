2c17730a8dc6048afa865406841673a12fa095a0
Yousef <yousef@Mac.fritz.box>
2026-08-10 10:01:09 +0200

    Solve LRU Cache

--- LeetCode/146. LRU Cache/146. LRU Cache/main.cpp
+++ LeetCode/146. LRU Cache/146. LRU Cache/main.cpp
@@ -11,47 +11,85 @@
 
 using namespace std;
 
+struct Node {
+    int key;
+    int value;
+    Node * next;
+    Node* prev;
+    
+    Node(int key, int value):
+    key(key), value(value), prev(nullptr), next(nullptr){}
+};
+
 class LRUCache {
   public:
-    LRUCache(int capacity) { this->capacity = capacity; }
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        this->head = new Node(0,0);
+        this->tail = new Node(0,0);
+        
+        this->tail->next = this->head;
+        this->head->prev = this->tail;
+    }
 
     int get(int key) {
         if (this->m.find(key) != m.end())
         {
-            this->last_used = key;
-            return m[key];
+            remove_node(m[key]);
+            add_node(m[key]);
+            return m[key]->value;
         }
         else
             return -1;
     }
 
     void put(int key, int value) {
-
-        //        if(this->m.find(key) !=m.end())
-        //        {
-        //            m[key] = value;
-        //            this->last_used = key;
-        //        }
-        if (m.size() >= capacity) {
-            m.erase(last_used);
+        
+        if(m.find(key) != m.end())
+        {
+            m[key]->value = value;
+            remove_node(m[key]);
+            add_node(m[key]);
+            return;
         }
-
-        m[key] = value;
-        this->last_used = key;
+        if (m.size() >= capacity) {
+            Node * tmp = this->tail->next;
+            m.erase(tmp->key);
+            remove_node(tmp);
+            delete tmp;
+        }
+        Node * node = new Node(key, value);
+        add_node(node);
+        m[key] = node;
+        
     }
+    
 
   private:
-    unordered_map<int, int> m;
+    unordered_map<int, Node *> m;
     int capacity;
-    int last_used = -1;
+    Node * head;
+    Node * tail;
+    
+    
+    void add_node(Node *node)
+    {
+        node->prev = this->head->prev;
+        node->next = this->head;
+        
+        this->head->prev->next = node;
+        this->head->prev = node;
+        
+    }
+    
+    void remove_node(Node * node)
+    {
+        node->prev->next= node->next;
+        node->next-> prev = node-> prev;
+    }
 };
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
+
 
 using namespace std;
 
